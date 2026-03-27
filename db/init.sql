cf3ec960f2465a99dd7628bf0ad8c8ef093f4db4
Soos <Salmonellaoverdose@gmail.com>
2026-03-27 09:15:08 +0100

    initialisation de program

---
+++ db/init.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS flaskdb;
+
+USE flaskdb;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email text
+);
+
+INSERT INTO users (name) VALUES ('Alice'), ('Bob'), ('Charlie');
+
